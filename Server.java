60a89fdb91da1e5398b5d4056b8f07d31b14dc56
Amit Manchella <74464072+amitmanchella@users.noreply.github.com>
2022-11-29 18:41:47 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,10 +1,13 @@
+import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+
 /**
  * A SearchServer class
  * It handles everything that deals with processing the search results. It also stores the data the search results are from.
+ *
  * @author saujinpark
  */
 public class Server {
@@ -21,34 +24,64 @@ public class Server {
 
         String choice = receive.readLine(); //reads if user creates an account or logs in
         User user = new User();
-        if (choice.equals(createLogIn[0])) {
-            //Verifying that creating an account is successful
-            String username = receive.readLine();
+        boolean hasTheAccountBeenCreated = false;
+            if (choice.equals(createLogIn[0])) {
+                //Verifying that creating an account is successful
+                do {
+
+                    String username = receive.readLine();
 
 
-            String password = receive.readLine();
+                    String password = receive.readLine();
 
-            String email = receive.readLine();
+                    String email = receive.readLine();
 
-            String buyerSeller = receive.readLine();
+                    String buyerSeller = receive.readLine();
 
-            if (buyerSeller.equalsIgnoreCase("buyer")) {
-                user = user.createAccount(username, email, password, 1);
-                accountCreated = true;
-                totalUsers.add(user);
-            } else if (buyerSeller.equalsIgnoreCase("seller")) {
-                user = user.createAccount(username, email, password, 2);
-                accountCreated = true;
-                totalUsers.add(user);
-            }
+                    //Receiver 01
 
 
-        } else if (choice.equals(createLogIn[1])) {
+                    if (buyerSeller.equalsIgnoreCase("buyer")) {
+                        try {
+                            user = user.createAccount(username, email, password, 1);
+                            String yVariable = "y";
+                            send.println(yVariable); // Reader on 54
+                            send.flush();
+                            hasTheAccountBeenCreated = true;
+                        } catch (Exception e) {
+                            String xVariable = "x";
+                            send.println(xVariable); // Reader on 54
+                            send.flush();
+
+                        }
+                    } else if (buyerSeller.equalsIgnoreCase("seller")) {
+                        try {
+                            user = user.createAccount(username, email, password, 2);
+                            String yVariable = "y";
+                            send.println(yVariable); // Reader on 54
+                            send.flush();
+                            hasTheAccountBeenCreated = true;
+                        } catch (Exception e) {
+                            String xVariable = "x";
+                            send.println(xVariable);
+                            send.flush();
+                        }
+                    }
+                } while (hasTheAccountBeenCreated == false);
+
+
+
+
+
+
+
+
+
+            } else if (choice.equals(createLogIn[1])) {
 
 
         }
-
-
-
     }
+
 }
+
