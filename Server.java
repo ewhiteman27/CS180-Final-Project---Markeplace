ebe584072b2446b78c854c62a01233af2dd53938
FishLovingDeveloper <111797579+FishLovingDeveloper@users.noreply.github.com>
2022-12-09 04:20:30 -0500

    Update Server.java
    I think im done

--- Server.java
+++ Server.java
@@ -691,6 +691,10 @@ public class Server {
                         String hi = "";
                     }
                 } else if (sellerResponse.equalsIgnoreCase(sellerOptions[9])) { //cart information
+                    ArrayList<String> cartStats = sell.;//insertmethod
+                    ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());  //OOS 1
+                    objectOutput.writeObject(cartStats);
+
 
                 }
             } while (whileSelling == false);
