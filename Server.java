0c1d6076eaf667906c9e36f4432c8e6a1d8f7f61
FishLovingDeveloper <111797579+FishLovingDeveloper@users.noreply.github.com>
2022-12-10 23:15:04 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -524,44 +524,29 @@ public class Server {
                     editProfile[2] = "Change Email";
 
                     String editChoice = receive.readLine(); //receiver 06
+                    if (editChoice.equalsIgnoreCase("n")) {
+                        String doNothing = "HAHA";
+
+                    }
 
                     if (editChoice.equals(editProfile[0])) { //username change
 
                         String emailStored = receive.readLine();
-
+                        //receiver 07
 
                         String passwordStored = receive.readLine();
-
+                        //receiver 08
 
                         String newUsernameStored = receive.readLine();
+                        //receiver 09
+                        if (emailStored == null || passwordStored == null || newUsernameStored == null) {
 
-
-                        boolean changeUsername = edits.changeUsername(newUsernameStored, passwordStored, emailStored);
-                        String confirmChange;
-
-                        if (changeUsername) {
-                            confirmChange = "true";
                         } else {
-                            confirmChange = "false";
-                        }
 
-                        send.println(confirmChange);
-                        send.flush();
-
-                    } else if (editChoice.equals(editProfile[1])) {  //password change
-                        String emailStored = receive.readLine();
-
-
-                        String usernameStored = receive.readLine();
-
-
-                        String newPasswordStored = receive.readLine();
-
-                        try {
-                            boolean changePassword = edits.changePassword(newPasswordStored, usernameStored, emailStored);
+                            boolean changeUsername = edits.changeUsername(newUsernameStored, passwordStored, emailStored);
                             String confirmChange;
 
-                            if (changePassword) {
+                            if (changeUsername) {
                                 confirmChange = "true";
                             } else {
                                 confirmChange = "false";
@@ -569,43 +554,75 @@ public class Server {
 
                             send.println(confirmChange);
                             send.flush();
+                            //sender 13
+                        }
 
-                        } catch (Exception e) {
-                            String confirmChange = "false";
-                            send.println(confirmChange);
-                            send.flush();
+                    } else if (editChoice.equals(editProfile[1])) {  //password change
+                        String emailStored = receive.readLine();
+                        //receiver 10
 
+                        String usernameStored = receive.readLine();
+                        //receiver 11
+
+                        String newPasswordStored = receive.readLine();
+                        //receiver 12
+
+                        if (emailStored == null || usernameStored == null || newPasswordStored == null) {
+
+                        } else {
+                            try {
+                                boolean changePassword = edits.changePassword(newPasswordStored, usernameStored, emailStored);
+                                String confirmChange;
+
+                                if (changePassword) {
+                                    confirmChange = "true";
+                                } else {
+                                    confirmChange = "false";
+                                }
+
+                                send.println(confirmChange);
+                                send.flush();
+                                //sender 14
+                            } catch (Exception e) {
+                                String confirmChange = "false";
+                                send.println(confirmChange);
+                                send.flush();
+                                //sender 14
+                            }
                         }
 
                     } else if (editChoice.equals(editProfile[2])) {  //emailchange
                         String usernameStored = receive.readLine();
-
+                        //receiver 15
 
                         String passwordStored = receive.readLine();
-
+                        //receiver 16
 
                         String newEmailStored = receive.readLine();
+                        //receiver 17
+                        if (usernameStored == null || passwordStored == null || newEmailStored == null) {
 
-                        String confirmChange;
-                        try {
-                            boolean changeEmail = edits.changeEmail(newEmailStored, usernameStored, passwordStored);
+                        } else {
+                            String confirmChange;
+                            try {
+                                boolean changeEmail = edits.changeEmail(newEmailStored, usernameStored, passwordStored);
 
-                            if (changeEmail) {
-                                confirmChange = "true";
-                            } else {
+                                if (changeEmail) {
+                                    confirmChange = "true";
+                                } else {
+                                    confirmChange = "false";
+                                }
+
+                            } catch (Exception e) {
                                 confirmChange = "false";
                             }
 
-                        } catch (Exception e) {
-                            confirmChange = "false";
+                            send.println(confirmChange);
+                            send.flush();
+                            //sender 18
                         }
 
-                        send.println(confirmChange);
-                        send.flush();
-
-
                     }
-
                 } else if (sellerResponse.equals(sellerOptions[1])) {
                     String usernameStored = receive.readLine();
 
